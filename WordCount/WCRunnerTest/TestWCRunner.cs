537cccc9432554a2aabd59bfaadf1d8310ea15ca
Jeremy Barrass <jeremy.barrass@gmail.com>
2016-06-17 16:52:40 +0100

    Test driving the refactor was not possible, as WCRunner had a protected status within the Solution.  However, a manual run post-refactor was successful.

---
+++ WordCount/WCRunnerTest/TestWCRunner.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using WordCount;
+using NUnit.Framework;
+
+namespace WCRunnerTest
+{
+	[TestFixture]
+	public class TestWCRunner
+	{
+		[SetUp]
+		public void BeforeEach()
+		{
+			WCRunner wcr = new WCRunner();
+		}
+	}
+}
+
--- WordCount/WordCount/WCRunner.cs
+++ WordCount/WordCount/WCRunner.cs
@@ -14,27 +14,14 @@ namespace WordCount
 			WCRunner wcr = new WCRunner ();
 			WordCounter wc = new WordCounter();
 			PrimeNum pn = new PrimeNum ();
-			string file;
 
-			try {
-				using  (StreamReader sr = new StreamReader(wcr.getFilePath())) {
-					file = sr.ReadToEnd();
-					wc.SetWordsSearchList (file);
-					wc.CountWords (wc.GetWordsSearchList ());
-
-					foreach (string key in wc.WordList.Keys) {
-						Console.WriteLine ("The word '{0}' appears {1} times, which {2} a prime number.", key, wc.WordList [key], pn.PrimeCheck (wc.WordList [key]) ? "is" : "is not");
-					}
-
-				} 
-			} catch (Exception e) {
-					Console.WriteLine("This file cannot be read.");
-					Console.WriteLine(e.Message);
-			}
+			wcr.LoadFile ();
+			wcr.ProcessFile (wcr.GetFile (), wc, pn);
 
 		}
 
 		private string filePath;
+		private string file;
 
 		public WCRunner(string filepath = "/home/jeremy/Dropbox/Github/CTM-tech-test/TestText.txt") {
 			filePath = filepath;
@@ -46,6 +33,34 @@ namespace WordCount
 
 		public void setFilePath(string fp) {
 			filePath = fp;
-		} 
+		}
+
+		private void SetFile(string f) {
+			file = f;
+		}
+
+		public string GetFile() {
+			return file;
+		}
+
+		public void LoadFile() {
+			try {
+				using  (StreamReader sr = new StreamReader(filePath)) {
+					SetFile(sr.ReadToEnd());
+				} 
+			} catch (Exception e) {
+				Console.WriteLine("This file cannot be read.");
+				Console.WriteLine(e.Message);
+			}
+		}
+
+		public void ProcessFile(string f, WordCounter wc, PrimeNum pn) {
+			wc.SetWordsSearchList (f);
+			wc.CountWords (wc.GetWordsSearchList ());
+
+			foreach (string key in wc.WordList.Keys) {
+				Console.WriteLine ("The word '{0}' appears {1} times, which {2} a prime number.", key, wc.WordList [key], pn.PrimeCheck (wc.WordList [key]) ? "is" : "is not");
+			}
+		}
 	}
 }
